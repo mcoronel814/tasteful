4479b51c2ac5403ad9c904368b6806e72ec2f0c8
Manuel Coronel <mcoronel814@gmail.com>
2023-08-07 16:29:22 -0400

    added homepage. all pages are up. all links working. firebase authentication is up too.

---
+++ hosting/src/main.js
@@ -0,0 +1,36 @@
+// Import our custom CSS
+import "./styles.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+//image toggling function
+/*
+
+var images = [
+  // "pictures/meal1.png",
+  // "pictures/meal3.png",
+  // "pictures/meal8.png",
+
+  "./src/meal1.png",
+  "./src/meal3.png",
+  "./src/meal8.png",
+];
+
+var imgElement = document.getElementById("pinkPicture");
+
+var currentImageIndex = 0;
+
+function toggleImage() {
+  imgElement.src = images[currentImageIndex];
+
+  currentImageIndex++;
+
+  if (currentImageIndex >= images.length) {
+    currentImageIndex = 0;
+  }
+}
+
+setInterval(toggleImage, 1000);
+
+*/
--- hosting/src/main.ts
+++ hosting/src/main.ts
@@ -1,24 +1,58 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, collection, CollectionReference, onSnapshot } from 'firebase/firestore'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const firebaseConfig = {
+  apiKey: "AIzaSyATnGOJTDgXe1hmwMY2lMEG-bsJGyUVxfU",
+  authDomain: "tasteful-48763.firebaseapp.com",
+  projectId: "tasteful-48763",
+  storageBucket: "tasteful-48763.appspot.com",
+  messagingSenderId: "347686408324",
+  appId: "1:347686408324:web:190251749ae113d26bb2d1",
+  measurementId: "G-BYQK03XM3E"
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+//auth
+const auth = getAuth();
+const button = document.querySelector('#signin');
+
+//db
+const db = getFirestore();
+const ul = document.querySelector('ul');
+
+
+onAuthStateChanged(auth, user => {
+  if(user == null) { return; }
+  const { uid } = user;
+  const personalRecipesCol = collection(db, `users/${uid}/personalRecipes`);
+  createStream(personalRecipesCol);
+});
+
+
+
+//realtime data
+function createStream(ref: CollectionReference) {
+  return onSnapshot(ref, snapshot => {
+    const personalRecipes = snapshot.docs.map(d => d.data());
+
+    /*
+    // sync with UI
+    personalRecipes.forEach(recipe => {
+      const li = document.createElement('li');
+      li.textContent = `${recipe.name} - ${recipe.instructions_and_notes}`;
+      ul?.appendChild(li);
+    })
+    */
+
+  });
+};
+
+
+button?.addEventListener('click', clickEvent => {
+  signInWithRedirect(auth, new GoogleAuthProvider());
+});
