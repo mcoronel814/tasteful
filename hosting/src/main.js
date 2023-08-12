3c4ea12646b38250c1e7debf56668a84a8ac65ef
SnootAlors <73448250+SnootAlors@users.noreply.github.com>
2023-08-12 10:45:07 -0400

    add ./ meal imgs

--- hosting/src/main.js
+++ hosting/src/main.js
@@ -5,22 +5,22 @@ import "./styles.scss";
 import * as bootstrap from "bootstrap";
 
 //image toggling function
-// var images = [
-//   // "pictures/meal1.png",
-//   // "pictures/meal3.png",
-//   // "pictures/meal8.png",
-//     "meal1.png",
-//     "meal3.png",
-//     "meal8.png",
-//     ];
-//     var imgElement = document.getElementById("pinkPicture");
-//     var currentImageIndex = 0;
+var images = [
+  // "pictures/meal1.png",
+  // "pictures/meal3.png",
+  // "pictures/meal8.png",
+    "./meal1.png",
+    "./meal3.png",
+    "./meal8.png",
+    ];
+    var imgElement = document.getElementById("pinkPicture");
+    var currentImageIndex = 0;
 
-//     function toggleImage() {
-//     imgElement.src = images[currentImageIndex];
-//     currentImageIndex++;
-//     if (currentImageIndex >= images.length) {
-//         currentImageIndex = 0;
-//     }
-// }
-// setInterval(toggleImage, 1000);
+    function toggleImage() {
+    imgElement.src = images[currentImageIndex];
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+}
+setInterval(toggleImage, 1000);
