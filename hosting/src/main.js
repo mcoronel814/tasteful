08263e1cc44c4630e56feee0c8897ed007e5d4ad
SnootAlors <73448250+SnootAlors@users.noreply.github.com>
2023-08-12 10:22:45 -0400

    add onclick wait

--- hosting/src/main.js
+++ hosting/src/main.js
@@ -5,35 +5,26 @@ import "./styles.scss";
 import * as bootstrap from "bootstrap";
 
 //image toggling function
-
-
 // var images = [
 //   // "pictures/meal1.png",
 //   // "pictures/meal3.png",
 //   // "pictures/meal8.png",
-
 //     "meal1.png",
 //     "meal3.png",
 //     "meal8.png",
 //     ];
-
 //     var imgElement = document.getElementById("pinkPicture");
-
 //     var currentImageIndex = 0;
 
 //     function toggleImage() {
 //     imgElement.src = images[currentImageIndex];
-
 //     currentImageIndex++;
-
 //     if (currentImageIndex >= images.length) {
 //         currentImageIndex = 0;
 //     }
 // }
-
 // setInterval(toggleImage, 1000);
 
 function wait() {
     document.getElementById("pinkPicture").src =  "./meal8.png" ;
 }
-setTimeout(wait, 4000);
