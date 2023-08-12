a5728081bab7e165972cc5b9630fc3b8efed0fb4
SnootAlors <73448250+SnootAlors@users.noreply.github.com>
2023-08-12 09:58:22 -0400

    try: ./meal.png, setTimeout

--- hosting/src/main.js
+++ hosting/src/main.js
@@ -7,28 +7,33 @@ import * as bootstrap from "bootstrap";
 //image toggling function
 
 
-var images = [
-  // "pictures/meal1.png",
-  // "pictures/meal3.png",
-  // "pictures/meal8.png",
+// var images = [
+//   // "pictures/meal1.png",
+//   // "pictures/meal3.png",
+//   // "pictures/meal8.png",
 
-    "meal1.png",
-    "meal3.png",
-    "meal8.png",
-    ];
+//     "meal1.png",
+//     "meal3.png",
+//     "meal8.png",
+//     ];
 
-    var imgElement = document.getElementById("pinkPicture");
+//     var imgElement = document.getElementById("pinkPicture");
 
-    var currentImageIndex = 0;
+//     var currentImageIndex = 0;
 
-    function toggleImage() {
-    imgElement.src = images[currentImageIndex];
+//     function toggleImage() {
+//     imgElement.src = images[currentImageIndex];
 
-    currentImageIndex++;
+//     currentImageIndex++;
 
-    if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
-    }
+//     if (currentImageIndex >= images.length) {
+//         currentImageIndex = 0;
+//     }
+// }
+
+// setInterval(toggleImage, 1000);
+
+function wait() {
+    document.getElementById("pinkPicture").src =  "./meal8.png" ;
 }
-
-setInterval(toggleImage, 1000);
+setTimeout(wait, 4000);
