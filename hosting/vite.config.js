385fa1443473098f77f7b581c644dec80e1859ae
SnootAlors <73448250+SnootAlors@users.noreply.github.com>
2023-08-12 13:41:41 -0400

    index in scr, del: head img src meal8

--- hosting/vite.config.js
+++ hosting/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'src/index.html'),
         about: resolve(__dirname, 'about/index.html'),
         aboutus: resolve(__dirname, 'aboutus/index.html'),
         map: resolve(__dirname, 'map/index.html'),
